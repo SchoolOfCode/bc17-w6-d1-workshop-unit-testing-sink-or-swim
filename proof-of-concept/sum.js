dcd77f0330d4d0930d3cdc5b079ce6300d8e99fb
Shilpa <eclectic89@hotmail.com>
2024-08-05 12:27:32 +0100

    Fixed some errors

---
+++ proof-of-concept/sum.js
@@ -0,0 +1,4 @@
+function sum(first, second) {
+    return (first + second);
+  }
+  export {sum};
--- proof-of-concept/sum_test.js
+++ proof-of-concept/sum_test.js
@@ -2,7 +2,7 @@ import {test, expect} from 'vitest';
 
 import {sum} from './sum.js';
 
-test1("sum of numbers",() => {
+test ("sum of numbers",() => {
   const result = sum(8, 9)  
-    expect(result).toBe(true);
+    expect(result).toBe(17);
 });
